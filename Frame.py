c28c4af044a163a141e7c912ddca66d1f104f019
lizizaid <lizizaid@gmail.com>
2022-04-07 17:24:05 +0300

    Added set_main_data

--- Frame.py
+++ Frame.py
@@ -1,3 +1,4 @@
+from FrameHeader import *
 from FrameSideInformation import FrameSideInformation
 
 NUM_PREV_FRAMES = 9
@@ -25,6 +26,7 @@ class Frame:
 
         starting_side_info_idx = 6 if header.crc == 0 else 4
         self.__side_info.set_side_info(self.__buffer[starting_side_info_idx:], header)
+        self.set_main_data(header, [0])
 
     # Determine the frame size.
     def __set_frame_size(self, header):
@@ -53,6 +55,49 @@ class Frame:
         self.frame_size = int(((samples_per_frame / 8) * header.bit_rate) / header.sampling_rate)
         if header.padding == 1:
             self.frame_size += 1
+
+    # Due to the Huffman bits' varying length the main_data isn't aligned with the frames.
+    # Unpacks the scaling factors and quantized samples.
+    def set_main_data(self, header: FrameHeader, last_buffer: list):
+        constant = 21 if header.channel_mode == ChannelMode.Mono else 36
+        if header.crc == 0:
+            constant += 2
+
+        # We'll put the main data in its own buffer. Main data may be larger than the previous frame and doesn't
+        # Include the size of side info and headers
+        if self.__side_info.main_data_begin == 0:
+            self.__main_data = self.__buffer[constant: self.frame_size - constant]
+        # TODO this requires getting last frame buffer from parser class
+        # else:
+        #     bound = 0
+        #     for frame in range(NUM_PREV_FRAMES):
+        #         bound += self.prev_frame_size[frame] - constant
+        #         if self.__side_info.main_data_begin < bound:
+        #             ptr_offset = self.__side_info.main_data_begin + frame * constant
+        #             buffer_offset = 0
+        #
+        #             part = [[0] * NUM_PREV_FRAMES]
+        #             part[frame] = self.__side_info.main_data_begin
+        #             for i in range(frame):
+        #                 part[i] = self.prev_frame_size[i] - constant
+        #                 part[frame] -= part[i]
+        #
+        #             self.__main_data = last_buffer[len(last_buffer) - ptr_offset: part[frame]]
+        #             ptr_offset -= (part[frame] + constant)
+        #             for i in range(frame-1, -1, -1):
+        #                 self.__main_data.extend(last_buffer[len(last_buffer) - ptr_offset: part[i]])
+        #                 ptr_offset -= (part[i] + constant)
+        #             self.__main_data.extend((self.__buffer[constant: self.frame_size - constant]))
+        #             break
+
+        # bit = 0
+        # for gr in range(2):
+        #     for ch in range(header.channels):
+        #         max_bit = bit + side_info.part2_3_length[gr][ch]
+        #         bit = unpack_scalefac(gr, ch, bit)
+        #         unpack_samples(gr, ch, bit, max_bit)
+        #         bit = max_bit
+
 
     @property
     def frame_size(self):
